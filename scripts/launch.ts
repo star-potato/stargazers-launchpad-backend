8261e83e18dbd560332e2e0b9de9dbac7f73da69
star-potato <stargazepotato@gmail.com>
2022-06-18 15:25:35 -0500

    ready to deploy

---
+++ scripts/launch.ts
@@ -0,0 +1,118 @@
+import { getClient } from '../src/client';
+import { toStars } from '../src/utils';
+
+const config = require('../config');
+const mnemonic:string = process.env.MNEMONIC!;
+
+async function burn_token(token: string, sg721:any) {
+  const client = await getClient(mnemonic);
+
+  console.log('SG721: ', sg721);
+  console.log('Burning Token: ', token);
+  const msg = { burn: { token_id: token } };
+  console.log(JSON.stringify(msg, null, 2));
+
+  const result = await client.execute(
+    config.account,
+    sg721,
+    msg,
+    'auto',
+    'burn'
+  );
+  const wasmEvent = result.logs[0].events.find((e) => e.type === 'wasm');
+  console.info(
+    'The `wasm` event emitted by the contract execution:',
+    wasmEvent
+  );
+}
+
+async function mintTo(recipient: string, minterAddress:string) {
+  const client = await getClient(mnemonic);
+
+  const starsRecipient = toStars(recipient);
+  console.log('Minter contract: ', minterAddress);
+  console.log('Minting to: ', starsRecipient);
+
+  const msg = { mint_to: { recipient: starsRecipient } };
+  console.log(JSON.stringify(msg, null, 2));
+  
+  const result = await client.execute(
+    config.account,
+    minterAddress,
+    msg,
+    'auto',
+    'mint to'
+  );
+  const wasmEvent = result.logs[0].events.find((e) => e.type === 'wasm');
+  console.info(
+    'The `wasm` event emitted by the contract execution:',
+    wasmEvent
+  );
+}
+
+async function queryInfo(tokenId:string, sg721:string){
+  const client = await getClient(mnemonic);
+  const tokenInfo = await client.queryContractSmart(sg721, {
+    all_nft_info: { token_id: tokenId },
+  });
+  console.log('tokenInfo:', tokenInfo);
+  return tokenInfo;
+}
+
+async function burn2mint1(rocketId:string, fuelId:string, starsAddress:string, minterAddress:string){
+  //query fuel owner
+  const responseQueryFuel = await queryInfo(fuelId, config.sg721FuelsTest);
+  console.log('responseQueryFuel.access.owner =', responseQueryFuel.access.owner);
+  console.log('starsAddress =', starsAddress);
+  console.log('approvals length = ', responseQueryFuel.access.approvals.length);
+  if (!starsAddress === responseQueryFuel.access.owner){
+    console.error('FUELNOTOWNER');
+    return 'FUELNOTOWNER';
+  }
+  if (responseQueryFuel.access.approvals.length === 0){
+    console.error('FUELNOAPPROVAL');
+    return 'FUELNOAPPROVAL';
+  }
+
+
+  //query rocket owner
+  const responseQueryRocket = await queryInfo(fuelId, config.sg721RocketsTest);
+  console.log('responseQueryRocket.access.owner', responseQueryRocket.access.owner);
+  console.log('starsAddress', starsAddress);
+  console.log('approvals length =', responseQueryRocket.access.approvals.length);
+  if (!starsAddress === responseQueryRocket.access.owner){
+    console.error('ROCKETNOTOWNER');
+    return 'ROCKETNOTOWNER';
+  }
+  if (responseQueryRocket.access.approvals.length === 0){
+    console.error('ROCKETNOAPPROVAL');
+    return 'ROCKETNOAPPROVAL';
+  }
+
+  //burn fuel
+  try {
+    const responseBurnFuel = await burn_token(fuelId, config.sg721FuelsTest);
+  } catch {
+    console.error('FUELBURNERROR');
+    return 'FUELBURNERROR';
+  }
+  //burn rocket
+  try {
+    const responseBurnRocket = await burn_token(rocketId, config.sg721RocketsTest);
+  } catch {
+    console.error ('ROCKETBURNERROR');
+    return 'ROCKETBURNERROR';
+  }
+  //mint new token
+  const responseMint = await mintTo(starsAddress, minterAddress);
+    console.error ('MINTSUCCESS');
+}
+
+const args = process.argv.slice(2);
+if (args.length == 0) {
+  console.log('No arguments provided, need rocketId, fuelId, starsAddress, minterAddress');
+} else if (args.length == 4) {
+  burn2mint1(args[0], args[1], args[2], args[3]);
+} else {
+  console.log('Invalid arguments');
+}
---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { exec } = require("child_process");
+const { stderr, stdout } = require("process");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  console.log("rocketId =", req.query.rocketId);
+  console.log("fuelId =", req.query.fuelId);
+  console.log("starsAddress =", req.query.starsAddress);
+  console.log("pfpMinter =", req.query.pfpMinter);
+  const command = "yarn launch " + req.query.rocketId + " " + req.query.fuelId + " " + req.query.starsAddress + " " + req.query.pfpMinter;
+  burnNMint(command, function (result){
+    console.log('result = ', result);
+    let finalResult = result.slice(45);
+    res.send(finalResult);
+  });
+  
+});
+
+function burnNMint (command, cb){
+  exec(command,
+    (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+        }
+        console.log(`stdout: ${stdout}`);
+        cb(stderr);
+    });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
