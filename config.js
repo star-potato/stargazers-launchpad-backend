f108a27291b6b7a8d38159bd47f825f8436351d3
star-potato <stargazepotato@gmail.com>
2022-06-30 12:54:33 -0500

    flip to mainnet

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ module.exports = {
 
   //// API CONFIG ////
   // The RPC endpoint for Stargaze, Double Double Testnet
-  rpcEndpoint: 'https://rpc.double-double-1.stargaze-apis.com/',
+  //rpcEndpoint: 'https://rpc.double-double-1.stargaze-apis.com/',
   // The RPC endpoint for Stargaze, mainnet
-  // rpcEndpoint: 'https://rpc.stargaze-apis.com/',
+  rpcEndpoint: 'https://rpc.stargaze-apis.com/',
   // NFT.storage endpoint
   nftStorageEndpoint: 'https://api.nft.storage',
   // NFT.storage API key
@@ -78,42 +78,42 @@ module.exports = {
   startTime: '2022-03-11T21:00:00.000Z',
   // The minter contract address
   // Get this after running `yarn minter`
-  minterHumans:'stars19equv33mget3ank0yxr5k3my7u9t6f7fakdy42g5rc4mv0ycaz3s3pfy3j',
-  sg721Humans: 'stars1ppnknq0xsmk9cd5frt7xujq6hqcdgg2q78f3hlwlj6a2kapeenlqfdl8zu',
+  minterHumans:'stars19pa0986q29kqcftj2l6m3d8a4v7j2gvjwmys5y07k8939wjyyzds958vn9',
+  sg721Humans: 'stars1ta25gf8tq2qc9pvphrtc9nrj79wykvg3yzhfqk48p2ncwzsgxp5qrmwl7p',
   humanImg:'https://stargaze.mypinata.cloud/ipfs/bafybeibr3f66jtzus5hsdpfuleu36a56gfbjwqccjteuo65lbnzm6njydm/images/',
-  minterDemons:'stars10l8a3uzxeqy955rk2h485cdswdtxvegdgzsr5cxuzlpah4l36t3q5msp3k',
-  sg721Demons: 'stars1gp8ykupusa8mle5sj0dmccgyaeelxw6s7sa3w7urtqgsc4ml503q74ttg4',
+  minterDemons:'stars19d5u0gt66j0u4fwg0vatnr4eht8xfzcasv6z0e65qrx653m9razq30at54',
+  sg721Demons: 'stars1cvl9x5lj9umelt45wgla8m4n68y4t03vhj488p5nlcs7eytjw79sqzd0kr',
   demonImg:'https://stargaze.mypinata.cloud/ipfs/bafybeidzmmxdvarfbmetgpguqnkarxebvaejb47m4gho4vjakpqsyydlii/images/',
-  minterCephalopods:'stars1tz5rc4lpl5rcud7hscgkfnnchf0x9nzgka8ccsnkjvp0utjdtvzqtmufw5',
-  sg721Cephalopods: 'stars1sdm0ran0eumwm999lyd0u045zafw7zku2rwcd948p2fcaakxatns9w0h8z',
+  minterCephalopods:'stars13n679843y9443rxjcrvuv3u588cftj8wd95e2zvuc2svl3c4908sm4pxmh',
+  sg721Cephalopods: 'stars1q895y7l6gsutenucljwwnwjhfpra54ulm3ahneh22ch9dtwp02wqvp5d4k',
   cephalopodImg:'https://stargaze.mypinata.cloud/ipfs/bafybeictjh2dz3aprhdc72pdwkjgdwydua7zmyuidadoj7ni5yk5pvjs74/images/',
-  minterEpics:'stars1k7pluj4gck66mrsmrntmcrhqgla2za0jj4pfsgq2ywxf6534stmqd7hmuj',
-  sg721Epics:'stars1cuhkffartyn6hpdrhp87sqrsar60mfv3t0klfvxp027gmrcwdl4qfqqdxj',
+  minterEpics:'stars1f07f287jqgqyy7qdcwazv3d34dtr27t3l3j59amt8e4zvz8a66rsse2zlv',
+  sg721Epics:'stars1ktgpf5szzcrq7v3v3e2tqaxe7wayu5nvy25ekxcete9xz47kgtxq4zq82n',
   epicImg:'https://stargaze.mypinata.cloud/ipfs/bafybeiaarlxmmsmfsdb2dg2azs24nodulzot5xjnlgjqu2mpg2xtn3mnxa/images/',
-  sg721Fuels:'stars1a2akm58cv8p0qk5yw398mm4zg3jw3ee5j8qyq9hzfwzpg07kgqqsfy4c2s',
-  sg721Rockets:'stars186tjfdguldrvrt6dcyqk0afwv5pcm8tl9y2p2qvjf36vfwlxnu5qys7pue',
-  tokenURL: 'https://testnet.publicawesome.dev/media/',
+  sg721Fuels:'stars18nj7a5p6g7agf76zyp2nrnv0fcjrfvsc82c5w7zzsdyjzgq0na3sfq92uz',
+  sg721Rockets:'stars12jj0nj3dk5uk4ldw4hkhx0y8ewx7ts53vfsamjjwyhdpjnwlh7xsyv3f74',
+  tokenURL: 'https://app.stargaze.zone/media/',
 
   // SG721 contract address
   // Get this after running `yarn minter`
 
   //// CONTRACT CODE IDs: Double Double Testnet ////
   // The code ID for sg721
-  sg721CodeId: 76,
+  //sg721CodeId: 76,
   // The code ID for the minter contract
-  minterCodeId: 75,
+  //minterCodeId: 75,
   // The code ID for the whitelist contract
-  whitelistCodeId: 3,
-  marketplace:
-    'stars154msvfvr3j37qpuqeq7neh00dh3dhzsf0cgflme9vwv7uqmjafns4dxrtr',
+  //whitelistCodeId: 3,
+  //marketplace:
+  //  'stars154msvfvr3j37qpuqeq7neh00dh3dhzsf0cgflme9vwv7uqmjafns4dxrtr',
 
   // //// CONTRACT CODE IDs: Mainnet ////
   // // The code ID for sg721
-  // sg721CodeId: 1,
+   sg721CodeId: 1,
   // // The code ID for the minter contract
-  // minterCodeId: 2,
+   minterCodeId: 2,
   // // The code ID for the whitelist contract
-  // whitelistCodeId: 3,
-  //   marketplace:
-  //       'stars1fvhcnyddukcqfnt7nlwv3thm5we22lyxyxylr9h77cvgkcn43xfsvgv0pl',
+   whitelistCodeId: 3,
+     marketplace:
+         'stars1fvhcnyddukcqfnt7nlwv3thm5we22lyxyxylr9h77cvgkcn43xfsvgv0pl',
 };
